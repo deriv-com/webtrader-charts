d60b59213fafab21a88e5d701d0bc4613e952874
amin roosta <amin.roosta@outlook.com>
2017-04-14 13:49:00 +0430

    fix indentations

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ var webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	devtool: 'source-map',
-	entry: './src/index.js',
-	output: {
-		filename: 'webtrader-charts.js',
-		path: path.resolve(__dirname, 'dist'),
-		// path: path.resolve(__dirname, '../src/webtrader-charts'),
-		library: 'WebtraderCharts',
-		libraryTarget: 'umd'
+   devtool: 'source-map',
+   entry: './src/index.js',
+   output: {
+      filename: 'webtrader-charts.js',
+      path: path.resolve(__dirname, 'dist'),
+      // path: path.resolve(__dirname, '../src/webtrader-charts'),
+      library: 'WebtraderCharts',
+      libraryTarget: 'umd'
    },
    externals: {
       'jquery' : {
@@ -21,45 +21,45 @@ module.exports = {
       }
    },
    module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
                   presets: ['env', 'stage-2'],
                   plugins: ['lodash'],
                },
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [ {
-					loader: 'html-loader',
-					options: { minimize: true }
-				}],
-			},
-			{ test: /\.svg/, use: [ "url-loader" ] },
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: "style-loader" /* creates style nodes from JS strings */ },
-					{ loader: "css-loader" /* translates CSS into CommonJS */ },
-					{ loader: "sass-loader" /* compiles Sass to CSS */ }
-				]
+            }
+         },
+         {
+            test: /\.html$/,
+            use: [ {
+               loader: 'html-loader',
+               options: { minimize: true }
+            }],
+         },
+         { test: /\.svg/, use: [ "url-loader" ] },
+         {
+            test: /\.scss$/,
+            use: [
+               { loader: "style-loader" /* creates style nodes from JS strings */ },
+               { loader: "css-loader" /* translates CSS into CommonJS */ },
+               { loader: "sass-loader" /* compiles Sass to CSS */ }
+            ]
          },
          {
             test: /\.json$/,
             use: 'json-loader'
          }
-		]
-	},
-	plugins: [
-		// new UglifyJSPlugin(),
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery'
-		})
-	]
+      ]
+   },
+   plugins: [
+      // new UglifyJSPlugin(),
+      new webpack.ProvidePlugin({
+         $: 'jquery',
+         jQuery: 'jquery'
+      })
+   ]
 };
