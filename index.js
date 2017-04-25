6374e6fc25ff73dd440af469178099f7d0ca878c
amin roosta <amin.roosta@outlook.com>
2017-04-25 21:27:23 +0430

    fix styles

--- index.js
+++ index.js
@@ -8,66 +8,38 @@ wtcharts.init({
    server: 'wss://ws.binaryws.com/websockets/v3'
 });
 
-_.delay(() => {
-   const $parent = $('#container');
+const $parent = $('#container');
 
-   wtcharts.chartWindow.addNewChart($parent, {
-     "instrumentCode": "RDBULL",
-     "instrumentName": "Bull Market Index",
-     "timePeriod": "1m",
-     "type": "line",
-     "delayAmount": 0,
-     "indicators": [
-       {
-         "id": "atr",
-         "name": "Average True Range",
-         "options": {
-           "levels": [
-             {
-               "color": "#FA6B0D",
-               "dashStyle": "Dash",
-               "label": { "text": 30 },
-               "value": 30,
-               "width": 1,
-             },
-             {
-               "color": "#FA6B0D",
-               "dashStyle": "Dash",
-               "label": { "text": 70 },
-               "value": 70,
-               "width": 1,
-             }
-           ],
-           "period": 14,
-           "strokeWidth": 1,
-           "stroke": "#FA6B0D",
-           "dashStyle": "Solid",
-           "appliedTo": 3
-         }
-       },
-       {
+const chart =  wtcharts.chartWindow.addNewChart($parent, {
+   "instrumentCode": "RDBULL",
+   "instrumentName": "Bull Market Index",
+   "timePeriod": "1m",
+   "type": "line",
+   "delayAmount": 0,
+   "indicators": [
+      {
          "id": "cks",
          "name": "Chande Kroll Stop",
          "options": {
-           "period": 10,
-           "maxMinPeriod": 20,
-           "multiplier": 3,
-           "longStopStroke": "#00C176",
-           "shortStopStroke": "#FF003C",
-           "strokeWidth": 1,
-           "dashStyle": "Solid"
+            "period": 10,
+            "maxMinPeriod": 20,
+            "multiplier": 3,
+            "longStopStroke": "#00C176",
+            "shortStopStroke": "#FF003C",
+            "strokeWidth": 1,
+            "dashStyle": "Solid"
          }
-       }
-     ],
-     "overlays": [
-       {
-         "symbol": "frxAUDJPY",
-         "displaySymbol": "AUD/JPY",
-         "delay_amount": 0
-       }
-     ],
-     "name": "1m linedot + Chande "
-   });
+      }
+   ],
+   // "overlays": [
+   //   {
+   //     "symbol": "frxAUDJPY",
+   //     "displaySymbol": "AUD/JPY",
+   //     "delay_amount": 0
+   //   }
+   // ],
 });
 
-console.warn('hello world');
+chart.events.anyChange = () => {
+   console.warn(chart.data());
+}; 
