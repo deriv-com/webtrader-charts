e321ffcd274be7303088f970248fcb589b0074a9
amin roosta <amin.roosta@outlook.com>
2017-04-26 21:52:21 +0430

    build new version

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
    entry: './src/index.js',
    output: {
       filename: 'webtrader-charts.js',
-      // path: path.resolve(__dirname, 'dist'),
-      path: path.resolve(__dirname, './example/node_modules/webtrader-charts/dist'),
+      path: path.resolve(__dirname, 'dist'),
+      // path: path.resolve(__dirname, './example/node_modules/webtrader-charts/dist'),
       // path: path.resolve(__dirname, '../src/webtrader-charts'),
       library: 'WebtraderCharts',
       libraryTarget: 'umd'
