332ceefa994ebd8499acf189d14768af48253eb4
amin roosta <amin.roosta@outlook.com>
2017-04-30 12:05:08 +0430

    remove datatables.net dependency

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-   devtool: 'source-map',
+   devtool: 'inline-source-map',
    entry: './src/index.js',
    output: {
       filename: 'webtrader-charts.js',
