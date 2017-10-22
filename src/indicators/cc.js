2f5246adc9d7a02549b69a3826f15b77738efe90
Eeo Jun <141bytes@gmail.com>
2017-10-22 13:43:06 +0100

    don't store on indicator instance

--- src/indicators/cc.js
+++ src/indicators/cc.js
@@ -10,28 +10,28 @@ window.CC = makeIndicator('cc', function(ind) {
     var indicators = ind.indicators;
 
     /* Coppock = WMA[10] of  (ROC[14] + ROC[11]).*/
-    ind.shortRoc = new ROC(data, { period: options.shortRocPeriod, appliedTo: options.appliedTo }, indicators);
-    ind.longRoc = new ROC(data, { period: options.longRocPeriod, appliedTo: options.appliedTo }, indicators);
+    var shortRoc = new ROC(data, { period: options.shortRocPeriod, appliedTo: options.appliedTo }, indicators);
+    var longRoc = new ROC(data, { period: options.longRocPeriod, appliedTo: options.appliedTo }, indicators);
 
     var rocData = data.map((tick, i) => ({
         time:  tick.time,
-        close: ind.shortRoc.indicatorData[i].value,
+        close: shortRoc.indicatorData[i].value + longRoc.indicatorData[i].value,
     }));
 
-    ind.wmaData = new WMA(rocData, { period: options.wmaPeriod }, indicators);
-    ind.indicatorData = ind.wmaData.indicatorData;
+    var wmaData = new WMA(rocData, { period: options.wmaPeriod }, indicators);
+    ind.indicatorData = wmaData.indicatorData;
 
     return {
         update: tick => {
-            var shortRoc = ind.shortRoc.update(tick)[0].value;
-            var longRoc = ind.longRoc.update(tick)[0].value;
-            var cc = ind.wmaData.update({ time: tick.time, close: shortRoc + longRoc })[0].value;
+            var short = shortRoc.update(tick)[0].value;
+            var long  = longRoc.update(tick)[0].value;
+            var cc = wmaData.update({ time: tick.time, close: short + long })[0].value;
             return [tick.time, cc];
         },
         next: tick => {
-            var shortRoc = ind.shortRoc.addPoint(tick)[0].value;
-            var longRoc = ind.longRoc.addPoint(tick)[0].value;
-            var cc = ind.wmaData.addPoint({ time: tick.time, close: shortRoc + longRoc })[0].value;
+            var short = shortRoc.addPoint(tick)[0].value;
+            var long = longRoc.addPoint(tick)[0].value;
+            var cc = wmaData.addPoint({ time: tick.time, close: short + long })[0].value;
             return [tick.time, cc];
         },
     };
