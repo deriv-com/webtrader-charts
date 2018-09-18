a2a99edaf8e0dcdcb2e18e1596ce408c2c4f655b
OskarAhl <oskar.ahlroth@gmail.com>
2018-09-18 13:42:28 +0800

    Update highstock-release to Highcharts

--- src/common/indicators.js
+++ src/common/indicators.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
-import Highcharts from 'highstock-release/highstock';
+import Highcharts from 'highcharts/highstock';
 import indicatorsArray from '../indicators-config.js';
 
 const indicatorsMetaData = _.cloneDeep(indicatorsArray);
